ac1d7b670d3cf64cc721fb0b678a0519be2910f9
Sree Grandhe <sgrandhe@utexas.edu>
2022-12-17 12:30:46 -0600

    Update server.go

--- backend/server.go
+++ backend/server.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	mongoClient "lazydependency/mongo_client"
+	neo4jClient "lazydependency/neo4j_client"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Print("Hello, World")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	mongoClient.InitMongoDB()
+	neo4jClient.InitNeo4j()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		log.Println("Receive Interrupt Signal")
+		mongoClient.CloseDriver()
+		neo4jClient.CloseDriver()
+
+		os.Exit(0)
+	}()
 }
