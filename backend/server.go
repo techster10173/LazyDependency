9c4b6457bc0f3e2671b4ea5ae926690049d0093d
Sree Grandhe <sgrandhe@utexas.edu>
2022-12-17 13:18:09 -0600

    Init Gin-Gonic Web Server

--- backend/server.go
+++ backend/server.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	mongoClient "lazydependency/mongo_client"
 	neo4jClient "lazydependency/neo4j_client"
+	router "lazydependency/router"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+	prod := flag.Bool("prod", false, "production-release mode")
+
+	if !(*prod) {
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file found")
+		}
 	}
 
 	mongoClient.InitMongoDB()
 	neo4jClient.InitNeo4j()
+
+	port, exists := os.LookupEnv("PORT")
+	if !exists || port == "" {
+		port = "80"
+	}
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router.InitServer(prod),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
