04e11ba0d3818af7fc2753744f307dfcce6a9bfc
Sree Grandhe <sgrandhe@utexas.edu>
2022-12-22 17:02:50 -0600

    Remove unused functions

--- backend/controllers/dependency.go
+++ backend/controllers/dependency.go
@@ -15,41 +15,6 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-// This gets a list of the versions that exist in the graph for this dependency
-func GetDependencyNode(c *gin.Context) {
-	// TODO Implement
-}
-
-// this gets a list of what other dependencies we have compability data on for this version (just what edges exist)
-func GetVersionNode(c *gin.Context) {
-	// TODO Implement
-}
-
-// this adds a new dependency node (if it doesnt already exist)
-func AddDependencyNode(c *gin.Context) {
-	// TODO Implement
-}
-
-// This adds a new version node to a given dependency node (if it doesnt already exist)
-func AddVersionNode(c *gin.Context) {
-	// TODO Implement
-}
-
-// this creates a new edge between two dependency/version combos
-func CreateEdge(c *gin.Context) {
-	// TODO Implement
-}
-
-// this gets the edge id between two dependency/versions and then gets the comments for this edge
-func GetEdge(c *gin.Context) {
-	// TODO Implement
-}
-
-// this adds a comment for a given edge (the comments consists of a comment, account/project id, and a 0 or 1 for a check or x)
-func AddComment(c *gin.Context) {
-	// TODO Implement
-}
-
 func UploadConnections(c *gin.Context) {
 	file, err := c.FormFile("file")
 	if err != nil {
